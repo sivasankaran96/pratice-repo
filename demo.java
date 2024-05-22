6220d0ddaa2989cd745c6c08448f2d536a561700
siva <gowrisiva@gmail.com>
2024-05-22 16:35:55 +0530

    Revert "Merge branch 'sivasankaran96-patch-1'"
    This reverts commit 753ce3b57f02432d5d3608414c12c01551784ffc, reversing
    changes made to a9a89fc160dbb48e1de385f9a3c654e425e1610d.

--- demo.java
+++ demo.java
@@ -1,12 +1,5 @@
 import json
-<<<<<<< HEAD
 
 with open("file.txt" ,'r') as fp:
 content = fp.read(15)
 	print("this is the content" ,content)
-=======
-  with open("demo.json" ,'r') as fp:
-  content = fp.read()
-    print ("this is the content " , content)
-fbfgh
->>>>>>> sivasankaran96-patch-1
